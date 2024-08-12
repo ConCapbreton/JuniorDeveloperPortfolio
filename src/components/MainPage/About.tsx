0944f92460ea34c7f10f5b87a8c90620aac22d70
ConCapbreton <connorsexton@hotmail.com>
2024-08-12 17:57:52 +0200

    imgUpdate5

--- src/components/MainPage/About.tsx
+++ src/components/MainPage/About.tsx
@@ -12,7 +12,7 @@ const {menuOff} = useMenuContext()
     <div id="about-div" onClick={menuOff}>
       <h1>Hi, I'm Martin Connor Sexton, <br></br>a Web Developer</h1>
       <div id="image-container">
-        <img src="/public/img/ProfilePic.JPG" alt="Profile Picture"></img>
+        <img src="/img/ProfilePic.JPG" alt="Profile Picture"></img>
         <p>I love exploring and creating, I'm a lifelong learner and I am passionate about living in the South West of France!<br /><br />Click here to see my <Link to="CV">CV!</Link> Or see below for my <a href="#projects-one">coding projects!</a></p>
       </div> 
       <div id="tech-icon-labels-holder">
